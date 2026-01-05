cc3d05d41cd9e40e1b958f5b4014861edbf8e392
Tim Metzler <tim.metzler@h-brs.de>
2026-01-05 13:43:59 +0100

    Add ts package

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,28 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+//import { IE2xCellPluginRegistry } from '@e2xgrader/cell-registry';
+import { E2xGraderCellRegistry } from '@e2xgrader/core';
+import { choiceCellPlugins } from './plugin';
+
 /**
  * Initialization data for the @e2xgrader/choice-cells extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: '@e2xgrader/choice-cells:plugin',
-  description: 'A JupyterLab extension that provides single and multiplechoice cells',
+  description:
+    'A JupyterLab extension that provides single and multiplechoice cells',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [E2xGraderCellRegistry.IE2xGraderCellRegistry],
+  activate: async (
+    app: JupyterFrontEnd,
+    cellRegistry: E2xGraderCellRegistry.IE2xGraderCellRegistry
+  ) => {
     console.log('JupyterLab extension @e2xgrader/choice-cells is activated!');
+    // Register the choice cell plugins
+    choiceCellPlugins.forEach(plugin => {
+      cellRegistry.registerPlugin(plugin);
+    });
   }
 };
 
